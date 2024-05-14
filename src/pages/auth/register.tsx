aca93dedcbc92f3423be710c4bf24f280e8a9f53
alvariz <alvarizanfal@gmail.com>
2024-05-14 20:15:02 +0900

    feat: register

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import RegisterView from "@/components/views/auth/Register";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <RegisterView></RegisterView>
+    </>
+  );
+};
+
+export default RegisterPage;
