b344f03a19f7560241efa11613390942fcf943b2
alvariz <alvarizanfal@gmail.com>
2024-05-26 17:23:32 +0900

    feat : diperlengkap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>google-site-verification=0mMWabqePyczIi4gMBn7asvxRe0WTBOPYKOQ_7rmyaY</Head>
+      <Head>
+        google-site-verification=0mMWabqePyczIi4gMBn7asvxRe0WTBOPYKOQ_7rmyaY
+        <meta name="google-site-verification" content="YourVerificationCodeHere" />
+      </Head>
       <body>
         <Main />
         <NextScript />
