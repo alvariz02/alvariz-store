52d706ef7f11c5345f44af83a212b5ede239df80
alvariz <alvarizanfal@gmail.com>
2024-05-26 17:18:06 +0900

    feat: google search

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>google-site-verification=0mMWabqePyczIi4gMBn7asvxRe0WTBOPYKOQ_7rmyaY</Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
